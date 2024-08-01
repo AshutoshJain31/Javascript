f23bde1f27b58be40dca8106145c591091e8958b
AshutoshJain31 <119740090+AshutoshJain31@users.noreply.github.com>
2024-08-01 13:07:12 +0000

    date type conversion and prefix and postfix operator

---
+++ 01_Basic/string_Conversion.js
@@ -0,0 +1,44 @@
+let score="30"
+let score1=30
+let score2=undefined
+let score3=null
+
+// console.log(typeof score3);
+
+//Data type conversion
+
+// let counter ="10"
+// console.log(typeof counter);
+// let stringcounter=Number(counter)
+// console.log(typeof stringcounter);
+// console.log(counter);
+
+// let counter =10
+// console.log(typeof counter);
+// let stringcounter=String(counter)
+// console.log(typeof stringcounter);
+// console.log(counter);
+
+// let counter =true
+// console.log(typeof counter);
+// let stringcounter=Number(counter)
+// console.log(typeof stringcounter);
+// console.log(stringcounter);
+//output
+// string
+// number
+// NaN
+
+// console.log("2"+3+4);
+// output===234
+
+// Prefix and postfix operator
+
+// let x=2
+// let y=x++;
+// console.log("x=",x,"y=",y);
+
+let x=2
+let y=++x;
+console.log("x=",x,"y=",y);
+

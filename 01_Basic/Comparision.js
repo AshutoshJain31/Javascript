e9477acbb34b899146b7eb7d5d45f0cf566773ac
AshutoshJain31 <119740090+AshutoshJain31@users.noreply.github.com>
2024-08-01 14:28:24 +0000

    Comparision operator

---
+++ 01_Basic/Comparision.js
@@ -0,0 +1,23 @@
+// Basic Comparision
+// console.log(2>3);
+//  console.log(2<3);
+//  console.log(2>=3);
+//  console.log(2<=3);
+//  console.log(2==2);
+
+//  different data type Comparision
+
+// console.log('5'>2);
+// console.log("2">=2);
+// note while we do comparision null with (<,>,>=,<=)comparision operator it will convert null to 0 and it equality(==) comparision it will not convert null  
+console.log(null>2);
+console.log(null<2);
+console.log(null==2);
+console.log(null>=2);
+console.log(null<=2);
+
+console.log(undefined>2);
+
+
+
+

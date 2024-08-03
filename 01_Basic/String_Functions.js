e92af6d8e79b484086fbbe206fa20838d15803b6
AshutoshJain31 <119740090+AshutoshJain31@users.noreply.github.com>
2024-08-03 11:06:54 +0000

    Final Changes

---
+++ 01_Basic/String_Functions.js
@@ -0,0 +1,33 @@
+//Description about the different function in the String    
+
+const name="ashutosh"
+
+console.log(name);
+
+const fullName=new String("Ashutosh Vijay Jain")
+
+console.log(fullName);
+console.log(typeof fullName);
+
+
+console.log(fullName.split(" "));
+
+console.log(fullName.toUpperCase());
+
+console.log(fullName.toLowerCase());
+
+console.log(fullName.length);
+console.log(fullName.charAt(2));
+console.log(fullName.indexOf("h"));
+console.log(fullName.bold());
+console.log(fullName.includes("Vijay"));
+
+
+
+
+
+
+
+
+
+

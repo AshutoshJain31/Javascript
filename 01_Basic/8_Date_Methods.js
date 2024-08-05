994c8bde8ca8f20b74eb27671e09bfbdeff3165a
AshutoshJain31 <119740090+AshutoshJain31@users.noreply.github.com>
2024-08-05 15:27:48 +0000

    Date Format

---
+++ 01_Basic/8_Date_Methods.js
@@ -0,0 +1,23 @@
+const date=new Date()
+
+console.log(date);
+
+const currentDate=date.getMonth()
+const time=date.getTime()
+const month=date.getTimezoneOffset()
+// const convert=date.toDateString()
+// const convert=date.toLocaleTimeString('en-IN')
+const year=date.getFullYear()
+const Month=date.getMonth()+1
+const day=date.getDate()
+const
+
+output.
+
+console.log(output);
+
+
+
+
+
+

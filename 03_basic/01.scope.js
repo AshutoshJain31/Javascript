54fda877df45a79c8751e5811b5e492c07ef3a81
AshutoshJain31 <119740090+AshutoshJain31@users.noreply.github.com>
2024-08-15 18:58:29 +0000

    scope Description

---
+++ 03_basic/01.scope.js
@@ -0,0 +1,51 @@
+// let number=function(){
+
+//     let a=10
+//     const b=20
+//     var c=30
+//     // console.log(a,b,c);
+    
+// }
+
+// number()
+// console.log(c);
+
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30
+// }
+
+// console.log(c);
+
+
+// const one=function(){
+//     const username="ashutosh"
+
+//     const two=function(){
+//         const website="ashutosh_Jain.com"
+//         console.log(website);
+        
+//     }
+//     two()
+//     console.log(username);
+    
+// }
+
+// one()
+// console.log(username);
+
+// ++++++++++++++++++++++++Important++++++++++++
+console.log(add(2,3));
+function add(num1,num2){
+    return num1+num2
+}
+
+
+console.log(ans);
+const addition=function(num1,num2){
+    return num1+num2
+}
+
+const ans=addition(2,4)
+

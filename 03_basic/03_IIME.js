df9c5c791e46d3dfc09482958526adb6e020832b
AshutoshJain31 <119740090+AshutoshJain31@users.noreply.github.com>
2024-08-17 08:15:59 +0000

    IIFE

---
+++ 03_basic/03_IIME.js
@@ -0,0 +1,13 @@
+
+//Immidiately Invoke Function Expression
+(function one(){
+    //IIME name function
+    console.log("Hello My name is Ashutosh");    
+})
+
+();
+// IIFE using Arrow Function
+((name)=>{
+    console.log(`my name is ${name}`);
+    
+})("Vijay")

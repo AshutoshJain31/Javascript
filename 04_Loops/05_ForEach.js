d5380bd8ff163d3b1063c44cb50f22cab3b45ed4
AshutoshJain31 <119740090+AshutoshJain31@users.noreply.github.com>
2024-08-24 13:16:49 +0000

    Foreach

---
+++ 04_Loops/05_ForEach.js
@@ -0,0 +1,31 @@
+const array1=['js','py','java','ruby','sql']
+
+// array1.forEach(function (item,index,arr){
+//     console.log(item,index,arr);
+    
+// })
+
+array1.forEach((key)=>{
+    // console.log(key);
+    
+})
+
+const coding=[
+    {
+        name:"ashutosh",
+        age:12
+    },
+    {
+        name:"Vijay",
+        age:20
+    },
+    {
+        name:"Vanita",
+        age:23
+    }
+]
+
+coding.forEach((key)=>{
+    console.log(key.name);
+    
+})
